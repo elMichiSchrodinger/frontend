f7be26daf9782ccbc47be5c685d56ca80428e14c
elMichiSchrodinger <darcklight47@gmail.com>
2024-11-16 00:25:18 -0500

    Proyecto DBD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {VacanteComponent} from './vacante/vacante.component';
+import {LoginComponent} from './login/login.component';
+import {PostulacionComponent} from './postulacion/postulacion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'vacante', component: VacanteComponent},
+  {path:'login', component: LoginComponent},
+  {path:'postulacion', component: PostulacionComponent}
+];
